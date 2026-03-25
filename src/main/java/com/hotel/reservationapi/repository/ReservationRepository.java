ae147dc6b3dbb39d77d35b5976c75341df4d0b12
Sahan <149289306+SahanWara@users.noreply.github.com>
2026-03-25 13:24:47 +0530

    Update ReservationRepository.java
    Added comments for better code readability

--- src/main/java/com/hotel/reservationapi/repository/ReservationRepository.java
+++ src/main/java/com/hotel/reservationapi/repository/ReservationRepository.java
@@ -3,6 +3,7 @@ package com.hotel.reservationapi.repository;
 import com.hotel.reservationapi.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// This method saves reservation to database
 public interface ReservationRepository
         extends JpaRepository<Reservation, Integer> {
 }
